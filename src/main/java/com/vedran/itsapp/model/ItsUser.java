06ce0042eccc07883948dc331c3a73f13c474ab6
Vedran Bradara <veca.bradara@gmail.com>
2019-03-02 15:15:09 +0100

    added user model

---
+++ src/main/java/com/vedran/itsapp/model/ItsUser.java
@@ -0,0 +1,9 @@
+package com.vedran.itsapp.model;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "users")
+public class ItsUser extends AbstractDocument {
+
+
+}
