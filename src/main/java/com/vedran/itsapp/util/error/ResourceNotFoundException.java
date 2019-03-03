b9babbcf7d68ae41b5163ed09d7e287046a7b888
Vedran Bradara <veca.bradara@gmail.com>
2019-03-03 17:25:25 +0100

    deleted all

--- src/main/java/com/vedran/itsapp/util/error/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.vedran.itsapp.util.error;
-
-public class ResourceNotFoundException extends RuntimeException {
-
-  public ResourceNotFoundException(Class model, String field, String value) {
-    super(String.format("%s with %s %s not found.",model.getSimpleName(),field,value));
-  }
-}
