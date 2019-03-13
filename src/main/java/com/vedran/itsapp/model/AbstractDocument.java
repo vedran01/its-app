1c3030f4a38d476f143fa5cd3e8d3fa869057191
Vedran Bradara <veca.bradara@gmail.com>
2019-03-13 10:09:45 +0100

    abstract document edit

--- src/main/java/com/vedran/itsapp/model/AbstractDocument.java
+++ src/main/java/com/vedran/itsapp/model/AbstractDocument.java
@@ -1,18 +1,23 @@
 package com.vedran.itsapp.model;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.LastModifiedDate;
 
 import java.util.Date;
 
-@Data
+@Getter
+@Setter
 public abstract class AbstractDocument {
   @Id
   private String id;
+
   @CreatedDate
   private Date created;
+
   @LastModifiedDate
   private Date modified;
 }
